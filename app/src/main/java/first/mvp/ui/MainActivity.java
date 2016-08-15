84390091c521ef7f963b969b905b98af2009673b
Ezequiel García <ezequiel.garcia@plyom.com.br>
2016-08-15 16:39:41 -0300

    bind buttons with butterknife

--- app/src/main/java/first/mvp/ui/MainActivity.java
+++ app/src/main/java/first/mvp/ui/MainActivity.java
@@ -3,15 +3,22 @@ package first.mvp.ui;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import first.mvp.view.GithubMVPView;
 import plyom.firstmvp.R;
 
 public class MainActivity extends AppCompatActivity implements GithubMVPView{
 
+    @BindView(R.id.button_clear) Button buttonClear;
+    @BindView(R.id.button_find) Button buttonFind;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
         setContentView(R.layout.activity_main);
     }
 
