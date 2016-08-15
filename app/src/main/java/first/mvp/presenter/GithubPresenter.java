3a4ccea83811a29e8093ae55b4dc43787ecc5de2
Ezequiel García <ezequiel.garcia@plyom.com.br>
2016-08-15 16:44:23 -0300

    implement methods

--- app/src/main/java/first/mvp/presenter/GithubPresenter.java
+++ app/src/main/java/first/mvp/presenter/GithubPresenter.java
@@ -1,8 +1,19 @@
 package first.mvp.presenter;
 
+import first.mvp.view.GithubMVPView;
+
 /**
  * Created by ezequiel.garcia on 8/15/16.
  */
 
-public class GithubPresenter {
+public class GithubPresenter implements Presenter<GithubMVPView> {
+    @Override
+    public void setView(GithubMVPView view) {
+
+    }
+
+    @Override
+    public void detachView() {
+
+    }
 }
